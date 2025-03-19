1a3037d2f0542ca63d7ab44e481b0a594a536db4
moriaso <muriasosalaon@gmail.com>
2025-03-19 09:42:04 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+// index.js
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${"Grace Hopper"}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
